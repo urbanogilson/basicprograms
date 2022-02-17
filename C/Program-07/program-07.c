912135ad2b1a83b7b57165b678505f465347c38b
Gilson Urbano <guf@hotmail.com.br>
2022-02-17 21:29:21 +0100

    Add main

--- C/Program-07/program-07.c
+++ C/Program-07/program-07.c
@@ -4,9 +4,20 @@ Write a hash table in C
 Inspired by: https://github.com/jamesroutley/write-a-hash-table
 */
 
+#include <stdio.h>
+
 #include "hash_table.h"
 
 int main(void) {
   ht_hash_table* hash_table = ht_new();
+  ht_insert(hash_table, "Gilson", "Urbano");
+  ht_insert(hash_table, "Brazil", "Denmark");
+  ht_insert(hash_table, "Age", "26");
+
+  printf("Gilson %s\n", ht_search(hash_table, "Gilson") ?: "Not found");
+  printf("Brazil -> %s\n", ht_search(hash_table, "Brazil" ?: "Not found"));
+  printf("Age: %s\n", ht_search(hash_table, "Age" ?: "Not found"));
+  printf("Address: %s\n", ht_search(hash_table, "Address") ?: "Not found");
+
   ht_del_hash_table(hash_table);
 }
