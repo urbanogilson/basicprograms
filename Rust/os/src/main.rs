de5687520b76dfb8061faba3bda3d6081eb9a165
Gilson Urbano <guf@hotmail.com.br>
2024-08-17 17:44:51 +0200

    Add tests

--- Rust/os/src/main.rs
+++ Rust/os/src/main.rs
@@ -1,19 +1,36 @@
 #![no_std]
 #![no_main]
-
-mod vga_buffer;
+#![feature(custom_test_frameworks)]
+#![test_runner(os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("Some panic messsage");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    os::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
---
+++ Rust/os/tests/basic_boot.rs
@@ -0,0 +1,26 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![reexport_test_harness_main = "test_main"]
+#![test_runner(os::test_runner)]
+
+use core::panic::PanicInfo;
+
+use os::println;
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    os::test_panic_handler(info)
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
