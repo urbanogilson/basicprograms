4656c21f9057424f8d02a903c15ad6526e53cf9c
Gilson Urbano <guf@hotmail.com.br>
2026-09-01 10:56:24 +0000

    cleanup

--- k8sbkp/prisma/migrations/20260706210713_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Agent" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Agent_pkey" PRIMARY KEY ("id")
-);
